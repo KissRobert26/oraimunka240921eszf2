1936de8ce617d9ba05cd23d384feb1fa2eb706fd
KissRobert26git config --global user.name KissRobert26git config --global user.name KissRobert26 <kiss.robert26@gmail.com>
2024-09-21 18:22:35 +0200

    elágazás minták

---
+++ bevf4.py
@@ -0,0 +1,21 @@
+# 4.	Osztályzat2: A program beolvas a konzolról egy egész számot!
+# Ha a szám 1 és 5 közötti, akkor legyen a beolvasott szám egy osztályzat!
+# A program írja ki a konzolra a számmal megadott osztályzatot szövegesen (1=elégtelen, …, 5=jeles)!
+# Ha a szám nem 1 és 5 közötti, akkor a program írja ki konzolra, hogy „érvénytelen osztályzat”!
+
+szam = int(input ("Kérem adjon meg egy egész számot!"))
+
+if (szam == 1):
+    print("elégtelen")
+elif (szam == 2):
+    print("elégséges")
+elif (szam == 3):
+    print("közepes")
+elif (szam == 4):
+
+    print("jó")
+elif (szam == 5):
+    print("jeles")
+
+else:
+    print("érvénytelen osztályzat")
